62aea22ffe4f9f25d614ace6c14880d6df83d11f
Kanishk Jain <142713282+Kanishk-03-Jain@users.noreply.github.com>
2023-12-18 21:28:03 +0530

    Add files via upload

--- scaling.py
+++ scaling.py
@@ -15,7 +15,12 @@ def Standardisation(data):
     scaled_data = data.copy()
     for column in data.columns:
         avg = data[column].mean()
-        std = statistics.stdev(data[column])
+        s=0
+        # for i in data[column]:
+        #     s += (i-avg)**2
+        # std = (s/len(data[column]))**0.5
+        std = np.std(data[column])
+        # std = statistics.stdev(data[column])
         scaled_data[column] = (data[column]-avg)/std
     return scaled_data
 data = pd.DataFrame({1:[1,2,3,4,5], 2:[6,7,8,9,10], 3:[11,12,13,14,15]})
